74bdf0aa4a5b442fa6d43840896be89f015d1ca1
simonrousseau <simonrousseau22@gmail.com>
2020-02-20 19:58:42 +0100

    Add

--- src/python/packages/solving/Algo.py
+++ src/python/packages/solving/Algo.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 
-
 class Algo:
-
     def __init__(self, problem):
+        self.allBooks = problem.allBooks
         self.day = problem.day
-       
+        self.libraries = problem.libraries
 
     def solve(self):
-        print(self.day)
+
+        # Initialisation
+        tempsRestant = self.day
+
+        # 1- Trier les librairies par score
+        
+        # 2- Supprimer les librairies inutiles
+
+        # 3- Boucle
+
+            # Inscrire la premiere
+            
+
+        print(self.allBooks)
